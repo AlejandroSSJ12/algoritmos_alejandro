4d41cadd6f054ada7cd5e545a6c08602066de3bd
AlejandroSSJ12 <darien_1321124089@uptecamac.edu.mx>
2021-10-06 15:02:00 -0500

    ultimo avance jiji

--- CLASE 3/main.js
+++ CLASE 3/main.js
@@ -6,17 +6,16 @@ var precio2 = "16";
 
 console.log(" Ir por el dinero con mi mamá :3 ");
 console.log("verificar si esta bonito el sol o esta lloviendo:");
-if ( clima > "sol") {
+if (clima > "sol") {
     console.log("Me voy caminando feliz:3");
-} else ( clima > "lluvia") {
-    console.log ("Nos vamos corriendo veloz porque nos mojamos");
+} else(clima > "lluvia") {
+    console.log("Nos vamos corriendo veloz porque nos mojamos");
 }
 
 console.log("Salir con el dinero para las tortillas");
-console.log( "verificar si me alcanza para el kilo de tortillas");
-if ( precio < "16"){
+console.log("verificar si me alcanza para el kilo de tortillas");
+if (precio < "16") {
     console.log("pedir el kilogramo completo");
-} else{
+} else {
     console.log("pedir lo que me alcance y regresar a mi casita");
 }
-
